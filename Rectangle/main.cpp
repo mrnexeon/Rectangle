1027a46c22c305c3d63c281bf2d8a4379356544d
Andrew Arkhipov <the.nexeon@gmail.com>
2017-10-06 16:22:27 +0300

    Initial Commit

---
+++ Rectangle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Rectangle
+//
+//  Created by Андрей Архипов on 06.10.2017.
+//  Copyright © 2017 Андрей Архипов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
